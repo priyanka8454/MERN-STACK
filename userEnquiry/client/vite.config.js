57694e55178be9be7aecd066d1ca2d1951a5cd21
priyanka8454 <1032241326@tcetmumbai.in>
2025-08-15 17:12:37 +0530

    vite.config.js

--- userEnquiry/client/vite.config.js
+++ userEnquiry/client/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import flowbite from 'flowbite/plugin'
 
 // https://vite.dev/config/
-
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    flowbite(),
-  ],
+  plugins: [react(), tailwindcss(),],
 })
